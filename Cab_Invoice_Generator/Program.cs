685a181492e24c6c29ff84aba6a43540bbf9b205
Arka <arka.spectacular@gmail.com>
2021-08-26 18:16:52 +0530

    [Arka]Add:Calculate No.of Rides and Average Fare

--- Cab_Invoice_Generator/Program.cs
+++ Cab_Invoice_Generator/Program.cs
@@ -10,7 +10,9 @@ namespace Cab_Invoice_Generator
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL_RIDE);
             Ride[] rides = { new Ride(2.0, 5), new Ride(6, 10) };
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
-            Console.WriteLine(summary.totalFare);
+            Console.WriteLine("Total Fare is : "+summary.totalFare);
+            Console.WriteLine("No of Rides are : " + summary.numOfRides);
+            Console.WriteLine("Average Fare is : " + summary.averageFare);
         }
     }
 }
